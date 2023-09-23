d30602075e577bbe26b0343f176aeddcfd835546
justin <justinmothupi@gmail.com>
2023-09-23 02:02:12 +0200

    C - Printf

--- handle_print.c
+++ handle_print.c
@@ -28,22 +28,29 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 
 	if (fmt_types[i].fmt == '\0')
 	{
-			if (fmt[*ind] == '\0')
-				return (-1);
-			unknow_len += write(1, "%%", 1);
-			if (fmt[*ind - 1] == ' ')
-				unknow_len += write(1, " ", 1);
-			else if (width)
-			{
+		if (fmt[*ind] == '\0')
+			return (-1);
+
+
+		unknow_len += write(1, "%%", 1);
+
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+
+		else if (width)
+		{
 			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
 				--(*ind);
 			if (fmt[*ind] == ' ')
 				--(*ind);
+
 			return (1);
 		}
+
 		unknow_len += write(1, &fmt[*ind], 1);
 		return (unknow_len);
 	}
+
 	return (printed_chars);
 }
